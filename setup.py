433ad9f4f040c73e24bb138db8d74d23e3d683eb
aster-777 <151450000+aster-777@users.noreply.github.com>
2023-11-21 01:16:27 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=[
-        'streamlit-lottie',
+        'streamlit_lottie',
         'PIL'
         'requests'
         'streamlit'
