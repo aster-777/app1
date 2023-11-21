80af94c472b63e2463f6f6ee98572b2a0be1c4af
aster-777 <151450000+aster-777@users.noreply.github.com>
2023-11-21 01:15:17 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='01_HOME',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit-lottie',
+        'PIL'
+        'requests'
+        'streamlit'
+        # Add other dependencies here
+    ],
+)
